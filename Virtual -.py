50bcfc8f7ac44709e31e7cbfcb31d097c3a04973
Machoolss <157117581+Machoolss@users.noreply.github.com>
2024-01-21 01:41:48 -0500

    Create Virtual -.py
    Virtual-.py es la nueva plataforma virtual para principiantes de programación python y HTML

---
+++ Virtual -.py
@@ -0,0 +1,7 @@
+echo "# Virtual-.py" >> README.md 
+git init 
+git add README.md 
+git commit -m "primer compromiso" 
+git rama -M main 
+git remoto agregar origen git@github.com:Machoolss/Virtual-.py. git
+ git push -u origen principal
